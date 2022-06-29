afb66f1f5194b72388291d9515255a2130d3220a
Jan <jspiewak@edu.cdv.pl>
2022-06-29 22:59:39 +0200

    Forget

--- Assets/_Game/Scripts/HP/EnemyHP.cs
+++ Assets/_Game/Scripts/HP/EnemyHP.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 using Photon.Pun;
 public class EnemyHP : LivingEntity
 {
+    private PhotonView _pv;
+    private void Awake()
+    {
+        _pv = GetComponent<PhotonView>();   
+    }
     public override void Die()
     {
-      
+        if (_pv.IsMine)
+        {
         PhotonNetwork.Destroy(this.gameObject);
+        }
     }
 }
